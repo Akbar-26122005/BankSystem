44552d5fbfb9658cbed1048355c5c52faf488be0
Akbar-26122005 <nurmatovakbar59@gmail.com>
2025-03-25 23:18:31 +0500

    Приложение доработано

--- BankSystem/MainWindow.xaml.cs
+++ BankSystem/MainWindow.xaml.cs
@@ -14,34 +14,79 @@ namespace BankSystem
 {
     public partial class MainWindow : Window
     {
+        Button? selectedButton = null;
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Grid_MouseEnter(object sender, MouseEventArgs e) {
-            Storyboard storyboard = (Storyboard)FindResource("AccountsBorderHoverLeaveStoryBoard");
+        private void sideMenu_MouseEnter(object sender, MouseEventArgs e) {
+            Storyboard storyboard = (Storyboard)FindResource("sideMenuHoverLeaveStoryBoard");
             storyboard.Stop();
 
-            storyboard = (Storyboard)FindResource("AccountsBorderHoverEnterStoryBoard");
+            storyboard = (Storyboard)FindResource("sideMenuHoverEnterStoryBoard");
             storyboard.Begin();
 
-            CornerRadius currentRadius = AccountsBorder.CornerRadius;
-            DoubleAnimation topLeftAnimation = new DoubleAnimation {
-                From = currentRadius.TopLeft,
-                To = 12,
-                Duration = new Duration(TimeSpan.FromSeconds(0.3))
-            };
-            AccountsBorder.CornerRadius = new CornerRadius(0, 10, 10, 0);
+            DoubleAnimation animation = new DoubleAnimation { To = 1.0, Duration = new Duration(TimeSpan.FromSeconds(0.3)) };
+            foreach (Button button in sideMenu_StackPanel.Children) {
+                FindVisualChild<TextBlock>(button).BeginAnimation(TextBlock.OpacityProperty, animation);
+            }
         }
 
-        private void Grid_MouseLeave(object sender, MouseEventArgs e) {
-            Storyboard storyboard = (Storyboard)FindResource("AccountsBorderHoverEnterStoryBoard");
+        private void sideMenu_MouseLeave(object sender, MouseEventArgs e) {
+            Storyboard storyboard = (Storyboard)FindResource("sideMenuHoverEnterStoryBoard");
             storyboard.Stop();
 
-            storyboard = (Storyboard)FindResource("AccountsBorderHoverLeaveStoryBoard");
+            storyboard = (Storyboard)FindResource("sideMenuHoverLeaveStoryBoard");
             storyboard.Begin();
-            AccountsBorder.CornerRadius = new CornerRadius(0);
+
+            DoubleAnimation animation = new DoubleAnimation { To = 0.0, Duration = new Duration(TimeSpan.FromSeconds(0.3)) };
+            foreach (Button button in sideMenu_StackPanel.Children) {
+                FindVisualChild<TextBlock>(button).BeginAnimation(TextBlock.OpacityProperty, animation);
+            }
+        }
+
+        private T FindVisualChild<T>(DependencyObject parent) where T : DependencyObject {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++) {
+                DependencyObject child = VisualTreeHelper.GetChild(parent, i);
+                if (child is T typedChild) {
+                    return typedChild;
+                }
+
+                T childOfChild = FindVisualChild<T>(child);
+                if (childOfChild != null) {
+                    return childOfChild;
+                }
+            }
+            return null;
+        }
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e) {
+            if (((Button)sender) == selectedButton) return;
+            SolidColorBrush brush = new SolidColorBrush(Colors.Transparent);
+            ((Button)sender).Background = brush;
+            ColorAnimation animation = new ColorAnimation { To = Colors.Gray, Duration = new Duration(TimeSpan.FromSeconds(0.3)) };
+            ((Button)sender).Background.BeginAnimation(SolidColorBrush.ColorProperty, animation);
+        }
+
+        private void Button_MouseLeave(object sender, MouseEventArgs e) {
+            ColorAnimation animation = new ColorAnimation { To = sender == selectedButton ? (Color)ColorConverter.ConvertFromString("#4A90E2") : Colors.Transparent,
+                Duration = new Duration(TimeSpan.FromSeconds(0.3)) };
+            ((Button)sender).Background.BeginAnimation(SolidColorBrush.ColorProperty, animation);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            if (selectedButton != null)
+                selectedButton.Background.BeginAnimation(SolidColorBrush.ColorProperty, new ColorAnimation { To = Colors.Transparent, Duration = new Duration(TimeSpan.FromSeconds(0.3)) });
+            selectedButton = (Button)sender;
+            ColorAnimation animation = new ColorAnimation { To = (Color)ColorConverter.ConvertFromString("#4A90E2"), Duration = new Duration(TimeSpan.FromSeconds(0.3)) };
+            ((Button)sender).Background.BeginAnimation(SolidColorBrush.ColorProperty, animation);
+        }
+
+        private void Button_MouseUp(object sender, MouseButtonEventArgs e) {
+            ColorAnimation animation = new ColorAnimation { Duration = new Duration(TimeSpan.FromSeconds(0.1)) };
+            animation.To = sender == selectedButton ? (Color)ColorConverter.ConvertFromString("#444") : Colors.Gray;
+            ((Button)sender).Background.BeginAnimation(SolidColorBrush.ColorProperty, animation);
         }
     }
 }
